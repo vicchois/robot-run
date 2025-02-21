a0db1c0a11cbf8d43b84fbaa080803ae5c8da2ff
jooshahn <joshuahminahn@gmail.com>
2025-02-21 13:18:24 -0800

    blue sky
    blue sky

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xadd8e6);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 6, 10);
 
